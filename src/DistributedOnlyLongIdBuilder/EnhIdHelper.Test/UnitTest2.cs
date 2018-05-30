92f3701b41d2055bd8f220f3333da8130eeeed9a
闵文辉 <mwh0808@qq.com>
2018-05-30 11:23:11 +0800

    update

---
+++ src/DistributedOnlyLongIdBuilder/EnhIdHelper.Test/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnhIdHelper.Test
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Guid guid = Guid.NewGuid();
+            byte[] bytes = guid.ToByteArray();
+
+            Console.WriteLine(guid);
+            Console.WriteLine(bytes);
+            Console.WriteLine(BitConverter.ToInt64(bytes, 0));
+
+            Assert.IsTrue(false);
+        }
+    }
+}
--- src/DistributedOnlyLongIdBuilder/EnhIdHelper/EnhIdWork.cs
+++ src/DistributedOnlyLongIdBuilder/EnhIdHelper/EnhIdWork.cs
@@ -20,12 +20,12 @@ namespace EnhEnhIdWork
 
         private static long twepoch = 687888001020L; //唯一时间随机量
 
-        private static long machineIdBits = 3L; //机器码字节数 ，（5位时为31，3位时为7）
-        private static long datacenterIdBits = 3L;//数据字节数 ，（5位时为31，3位时为7）
+        private static long machineIdBits = 3L; //机器码字节数 ，（5位时为31，3位时为7），原始默认值为5L
+        private static long datacenterIdBits = 3L;//数据字节数 ，（5位时为31，3位时为7），原始默认值为5L
         public static long maxMachineId = -1L ^ -1L << (int)machineIdBits; //最大机器ID
         public static long maxDatacenterId = -1L ^ (-1L << (int)datacenterIdBits);//最大数据ID
 
-        private static long sequenceBits = 12L; //计数器字节数，12个字节用来保存计数码        
+        private static long sequenceBits = 12L; //计数器字节数，12个字节用来保存计数码   ，原始默认值12L     
         private static long machineIdShift = sequenceBits; //机器码数据左移位数，就是后面计数器占用的位数
         private static long datacenterIdShift = sequenceBits + machineIdBits;
         private static long timestampLeftShift = sequenceBits + machineIdBits + datacenterIdBits; //时间戳左移动位数就是机器码+计数器总字节数+数据字节数
@@ -33,7 +33,7 @@ namespace EnhEnhIdWork
         private static long lastTimestamp = -1L;//最后时间戳
 
         private static object syncRoot = new object();//加锁对象
-        static EnhIdWork snowflake;
+        private static EnhIdWork snowflake;
         
         public static EnhIdWork Instance()
         {
@@ -42,6 +42,7 @@ namespace EnhEnhIdWork
             return snowflake;
         }
 
+        #region 构造函数
         public EnhIdWork()
         {
             Snowflakes(0L, -1);
@@ -55,9 +56,11 @@ namespace EnhEnhIdWork
         public EnhIdWork(long machineId, long datacenterId)
         {
             Snowflakes(machineId, datacenterId);
-        }
+        } 
+        #endregion
 
         private void Snowflakes(long machineId, long datacenterId)
+        #region MyRegion
         {
             if (machineId >= 0)
             {
@@ -65,27 +68,33 @@ namespace EnhEnhIdWork
                 {
                     throw new Exception("机器码ID非法");
                 }
+
                 EnhIdWork.machineId = machineId;
             }
+
             if (datacenterId >= 0)
             {
                 if (datacenterId > maxDatacenterId)
                 {
                     throw new Exception("数据中心ID非法");
                 }
+
                 EnhIdWork.datacenterId = datacenterId;
             }
-        }
+        } 
+        #endregion
 
         /// <summary>
         /// 生成当前时间戳
         /// </summary>
         /// <returns>毫秒</returns>
         private static long GetTimestamp()
+        #region MyRegion
         {
-            //让他2000年开始
+            //让他2000年前开始
             return (long)(DateTime.UtcNow - new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
-        }
+        } 
+        #endregion
 
         /// <summary>
         /// 获取下一微秒时间戳
@@ -93,9 +102,11 @@ namespace EnhEnhIdWork
         /// <param name="lastTimestamp"></param>
         /// <returns></returns>
         private static long GetNextTimestamp(long lastTimestamp)
+        #region MyRegion
         {
             long timestamp = GetTimestamp();
             int count = 0;
+
             while (timestamp <= lastTimestamp)//这里获取新的时间,可能会有错,这算法与comb一样对机器时间的要求很严格
             {
                 count++;
@@ -104,18 +115,22 @@ namespace EnhEnhIdWork
                 Thread.Sleep(1);
                 timestamp = GetTimestamp();
             }
+
             return timestamp;
-        }
+        } 
+        #endregion
 
         /// <summary>
         /// 获取长整形的ID
         /// </summary>
         /// <returns></returns>
         public long GetId()
+        #region MyRegion
         {
             lock (syncRoot)
             {
                 long timestamp = GetTimestamp();
+
                 if (EnhIdWork.lastTimestamp == timestamp)
                 { //同一微妙中生成ID
                     sequence = (sequence + 1) & sequenceMask; //用&运算计算该微秒内产生的计数是否已经到达上限
@@ -130,18 +145,22 @@ namespace EnhEnhIdWork
                     //不同微秒生成ID
                     sequence = 0L;
                 }
+
                 if (timestamp < lastTimestamp)
                 {
                     throw new Exception("时间戳比上一次生成ID时时间戳还小，故异常");
                 }
+
                 EnhIdWork.lastTimestamp = timestamp; //把当前时间戳保存为最后生成ID的时间戳
+
                 long Id = ((timestamp - twepoch) << (int)timestampLeftShift)
                     | (datacenterId << (int)datacenterIdShift)
                     | (machineId << (int)machineIdShift)
                     | sequence;
+
                 return Id;
             }
-        }
-
+        } 
+        #endregion
     }
 }
