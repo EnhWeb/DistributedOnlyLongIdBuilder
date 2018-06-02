e23557c8fb540f44fefdbe4a1cbd67e8f1fbe137
闵文辉 <mwh0808@qq.com>
2018-06-02 13:32:33 +0800

    update

---
+++ src/DistributedOnlyLongIdBuilder/EnhIdHelper.Test/UnitTest5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnhIdHelper.Test
+{
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
